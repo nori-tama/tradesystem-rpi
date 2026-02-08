48177beb1e331b564b904704a78cd7837f4ff456
nori-tama <example@gmail.com>
2026-02-09 00:47:38 +0900

    SQLiteからMySQLへのデータベース設定を変更

--- django/tradesystem_web/settings.py
+++ django/tradesystem_web/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'tradesystem_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tradesystem',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
